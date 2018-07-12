845dd597e3e7efca269ce4ce6a231cb7e3d8b3f4
Felipe Soares <felipeluizsoares@gmail.com>
2018-07-12 01:41:08 -0300

    Update Users component to use the Query from react apollo

--- src/screens/Users.js
+++ src/screens/Users.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { View, Text, Button } from 'react-native'
 
-import { graphql } from 'react-apollo'
+import { Query } from 'react-apollo'
 
 import { USERS } from '../queries/users'
 
@@ -11,28 +11,29 @@ class Users extends React.Component {
   }
 
   render() {
-    const { data } = this.props
-
     return (
-      <View>
-        {
-          data && data.users && data.users.map(user =>
-            <View key={user.id}>
-              <Text>{user.name}</Text>
-              <Text>{user.email}</Text>
-              <Text>{user.phone}</Text>
+      <Query query={USERS}>
+        {({ loading, error, data }) => {
+          if (loading) return <Text>Loading..</Text>
+          if (error) return <Text>{`Error!: ${error}`}</Text>
+
+          return (
+            <View>
+              {
+                data.users.map(user =>
+                  <View key={user.id}>
+                    <Text>Name: {user.name}</Text>
+                    <Text>E-mail: {user.email}</Text>
+                    <Text>Phone: {user.phone}</Text>
+                  </View>
+                )
+              }
             </View>
           )
-        }
-        {
-          data && data.loading && <Text>Loading..</Text>
-        }
-        {
-          data && data.error && <Text>{data.error.trace}</Text>
-        }
-      </View>
+        }}
+      </Query>
     )
   }
 }
 
-export default graphql(USERS)(Users)
+export default Users
